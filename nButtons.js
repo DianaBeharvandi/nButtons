ba4fb71e910e0c93ff7b25e8a287223cdd00ef08
DianaBeharvandi <94246585+DianaBeharvandi@users.noreply.github.com>
2022-02-07 11:34:27 +0200

    Add files via upload
    v5

--- nButtons.js
+++ nButtons.js
@@ -1,5 +1,5 @@
 let winningButton;
-function getInputValue(){
+function getNumberOfButtons() {
   var inputVal = document.getElementById("myInput").value;          
   let n = inputVal;
   winningButton = Math.ceil(Math.random() * n);
